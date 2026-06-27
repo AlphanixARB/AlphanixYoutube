a12335dab1277e4f65655ea43f2459ebddbac256
Alphanix <128967156+AlphanixARB@users.noreply.github.com>
2026-06-27 11:19:37 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,21 @@
 pluginManagement {
     repositories {
-//        google {
-//            content {
-//                includeGroupByRegex("com\\.android.*")
-//                includeGroupByRegex("com\\.google.*")
-//                includeGroupByRegex("androidx.*")
-//            }
-//        }
-        maven { url = uri("https://maven.myket.ir/") }
-//        mavenCentral()
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-//        google()
-//        mavenCentral()
-        maven { url = uri("https://maven.myket.ir/") }
+        google()
+        mavenCentral()
     }
 }
 
